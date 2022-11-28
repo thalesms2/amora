cb18acbf7b03465d5e7b407a027ffe7352d91d91
Thales Minoru Sato <thalesms2@outlook.com>
2022-11-28 18:04:01 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://mulberry.vercel.app",
-    baseURL: "http://localhost:4000",
+    baseURL: "https://mulberry.vercel.app",
+    // baseURL: "http://localhost:4000",
     headers: {
         "Content-Type": "application/json"
     }
