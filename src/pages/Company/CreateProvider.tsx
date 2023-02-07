f43e600577fb192a89c23894bf48fbdc9aa9dc1e
Thales Minoru Sato <thalesms2@outlook.com>
2023-02-07 15:28:50 -0300

    feat: progress on create provider

--- src/pages/Company/CreateProvider.tsx
+++ src/pages/Company/CreateProvider.tsx
@@ -7,6 +7,22 @@ import {
     Autocomplete,
     Button,
 } from "@mui/material";
+import { toast } from "react-toastify"
+import { usePopup } from '../../hooks/usePopup';
+import promiseResults from '../../hooks/toastPromiseDefault';
+import cepApi from '../../hooks/consultCep';
+import api from '../../hooks/api';
+
+
+interface City {
+    code: number;
+    name: string;
+}
+
+interface State {
+    code: string;
+    name: string;
+}
 
 const CreateProvider: React.FC = () => {
     const [id, setId] = React.useState<string>('')
@@ -19,11 +35,116 @@ const CreateProvider: React.FC = () => {
     const [neighborhood, setNeighborhood] = React.useState<string>('')
     const [number, setNumber] = React.useState<string>('')
     const [complement, setComplement] = React.useState<string>('')
-    function handleClear() {
-        return 0
+    const [cityValue, setCityValue] = React.useState("");
+    const [cityCode, setCityCode] = React.useState<Number>(null);
+    const [stateValue, setStateValue] = React.useState("");
+    const [stateCode, setStateCode] = React.useState<String>('');
+    const [citys, setCitys] = React.useState<City[]>([]);
+    const [states, setStates] = React.useState<State[]>([]);
+    const [create, setCreate] = usePopup();
+    
+    const numRef = React.useRef(null)
+
+    async function handleSubmit() {
+        try {
+            await toast.promise(api.post("/provider/create", {
+                id: Number(id),
+                socialName: String(socialName),
+                fantasyName: String(fantasy),
+                cnpj: String(cnpj),
+                cityCode: Number(cityCode),
+                cep: String(cep),
+                neighborhood: String(neighborhood),
+                street: String(street),
+                adressNumber: String(number),
+                complement: String(complement),
+                userId: window.sessionStorage.getItem('userId')
+            }), promiseResults)
+            toast('Client created 🥳')
+            handleClear()
+        } catch (err) {
+            toast('Error 😦')
+        }
     }
-    function handleSubmit() {
-        return 0
+    function handleClear() {
+        setId('')
+        setSocialName('')
+        setFantasy('')
+        setCnpj('')
+        setCityCode(null)
+        setCityValue('')
+        setStateCode(null)
+        setStateValue('')
+        setCep('')
+        setNeighborhood('')
+        setStreet('')
+        setNumber('')
+        setComplement('')
+    }
+
+    function checkPress(e) {
+        if(e.keyCode == 13 || e.keyCode == 9) {
+            e.preventDefault()
+            numRef.current.focus()
+        }
+    }
+    async function getCitys(stateCode) {
+        try {
+            const { data } = await api.get(`/city/all/${stateCode}`);
+            const res: any = [];
+            data.map((city: City) => {
+                res.push({
+                    key: `${city.code} - ${city.name}`,
+                    label: city.name,
+                    content: city.code,
+                });
+            });
+            toast("Got the citys 🥳");
+            setCitys(res);
+        } catch (err) {
+            toast("Error to get citys 😦");
+        }
+    }
+    async function getStates() {
+        try {
+            const { data } = await api.get("/state");
+            const res: any = [];
+            data.map((state: State) => {
+                res.push({
+                    key: `${state.code} - ${state.name}`,
+                    label: state.name,
+                    content: state.code,
+                });
+            });
+            toast("Got the states 🥳");
+            setStates(res);
+        } catch (err) {
+            toast("Error to get states 😦");
+        }
+    }
+    React.useEffect(() => {
+        getStates();
+    }, []);
+    async function selectedStateUpdateCitys(state) {
+        setStateCode(state.content);
+        setStateValue(state.label);
+        getCitys(state.content)
+    }
+    async function handleCaptureCep() {
+        const { data } = await toast.promise(cepApi.get(`/${cep}/json`), promiseResults)
+        setStateCode(data.uf);
+        const searchStateLabel = states.map((state: any) => {
+            if (state.content == data.uf) return state.label
+        })
+        selectedStateUpdateCitys({content: 'SC' , label: 'Santa Catarina'})
+        setStateValue(String(searchStateLabel));
+        setCityValue(data.localidade)
+        const searchCityCode = citys.map((city:any) => {
+            if(city.label == data.localidade) return city.content
+        })
+        setCityCode(Number(searchCityCode));
+        setNeighborhood(data.bairro)
+        setStreet(data.logradouro)
     }
     return (
         <Box
@@ -34,7 +155,7 @@ const CreateProvider: React.FC = () => {
                 alignContent: "center",
             }}
         >
-            <Typography variant="h3">Create a new Client</Typography>
+            <Typography variant="h3">Create a new Provider</Typography>
             <Paper
                 elevation={6}
                 sx={{
@@ -72,7 +193,7 @@ const CreateProvider: React.FC = () => {
                         sx={{
                             marginBottom: "1vw",
                             marginRight: "1vw",
-                            width: "35vw"
+                            width: "25vw"
                         }}
                     />
                     <TextField 
@@ -99,7 +220,7 @@ const CreateProvider: React.FC = () => {
                         sx={{
                             marginBottom: "1vw",
                             marginRight: "1vw",
-                            width: "41vw"
+                            width: "36vw"
                         }}
                     />
                     <TextField 
@@ -109,7 +230,7 @@ const CreateProvider: React.FC = () => {
                         onChange={(e) => setIe(e.target.value)}
                         sx={{
                             marginBottom: "1vw",
-                            width: "15vw",
+                            width: "10vw",
                         }}
                     />
                 </Box>
@@ -123,11 +244,62 @@ const CreateProvider: React.FC = () => {
                         value={cep}
                         variant="standard"
                         onChange={(e) => setCep(e.target.value)}
+                        onKeyDown={(e) => checkPress(e)}
+                        onBlur={(e) => handleCaptureCep()}
                         sx={{
                             marginBottom: "1vw",
                             marginRight: "1vw",
-                            width: "15vw",
+                            width: "8vw",
                         }}
+                    />
+                    <Autocomplete
+                        disablePortal
+                        disableClearable
+                        options={states}
+                        value={stateValue}
+                        isOptionEqualToValue={(option, value) =>
+                            option.id === value.id
+                        }
+                        sx={{
+                            width: "12vw",
+                            marginBottom: "1vw",
+                            marginRight: "1vw",
+                        }}
+                        onChange={(event: any, state: any) => {
+                            selectedStateUpdateCitys(state)
+                        }}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="States"
+                                variant="standard"
+                            ></TextField>
+                        )}
+                    />
+                    <Autocomplete
+                        disablePortal
+                        disableClearable
+                        options={citys}
+                        value={cityValue}
+                        isOptionEqualToValue={(option, value) =>
+                            option.id === value.id
+                        }
+                        sx={{
+                            width: "10vw",
+                            marginBottom: "1vw",
+                            marginRight: "1vw",
+                        }}
+                        onChange={(event: any, city: any) => {
+                            setCityCode(city.content);
+                            setCityValue(city.label);
+                        }}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Citys"
+                                variant="standard"
+                            ></TextField>
+                        )}
                     />
                     <TextField 
                         label="Street"
@@ -137,13 +309,14 @@ const CreateProvider: React.FC = () => {
                         sx={{
                             marginBottom: "1vw",
                             marginRight: "1vw",
-                            width: "35vw"
+                            width: "20vw"
                         }}
                     />
                     <TextField 
                         label="Number"
                         value={number}
                         variant="standard"
+                        inputRef={numRef}
                         onChange={(e) => setNumber(e.target.value)}
                         sx={{
                             marginBottom: "1vw",
